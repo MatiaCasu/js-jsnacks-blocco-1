0ffd9d30e04a4f8638c861c15126f1cd3eaf0027
Matia <matia.casu@gmail.com>
2021-03-04 18:47:03 +0100

    Es8 stampato in Lista su schermo

--- snack8/js/script.js
+++ snack8/js/script.js
@@ -8,5 +8,9 @@ for (var i = 0; i < 4; i++) {
   n[i] = i;
   add += parseInt(n[i]);
 }
-console.log(n);
-console.log(add);
+
+n = "<li>" + "Il tuo numero è: " + n + "</li>"
+add = "<li>" + "La somma delle sue cifre è: " + add + "</li>";
+
+var ulList = "<ul>" + n + add + "</ul>";
+document.getElementById("ulList").innerHTML = ulList;
